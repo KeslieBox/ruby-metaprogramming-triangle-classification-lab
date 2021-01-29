11bf5df1838b0f0f803e70b22990b7c9a59b410f
KeslieBox <kesliebox@gmail.com>
2021-01-28 22:29:36 -0600

    commit tree 0aa89b88d2e7f1e268827d319c3773cc7ea26b88

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Triangle
-  # write code here
+ 
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    # binding.pry
+    if @a+@b <= @c || @b+@c <= @a || @a+@c <= @b || [@a,@b,@c].find{|i| i==0 || i <0} 
+        begin
+          raise TriangleError
+        end
+    elsif @a==@b && @b==@c
+      :equilateral
+    elsif @a==@b || @b==@c || @a==@c
+      :isosceles
+    elsif [@a,@b,@c].max < [@a+@b, @a+@c, @b+@c].max && @a!=@b && @b!=@c
+      :scalene
+    end
+  end
+
+
+  
+  
+  class TriangleError < StandardError
+    def message
+      "this is not a triangle"
+    end
+  end
 end
